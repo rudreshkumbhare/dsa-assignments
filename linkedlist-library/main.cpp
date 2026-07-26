4f0f220ce3091157d2bc89e3740b809284ee665f
rudresh <rudresh.kumbhare25@pccoepune.org>
2026-07-26 19:23:40 +0530

    Main to test SLL, DLL and CLL functionality

--- linkedlist-library/main.cpp
+++ linkedlist-library/main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include "SLL.h"
 #include "DLL.h"
+#include "CLL.h"
 using namespace std;
 
 int main() {
-    cout << "     Testing Singly Linked List (SLL)   \n";
+    cout << "========================================\n";
+    cout << "   Testing Singly Linked List (SLL)     \n";
+    cout << "========================================\n";
     SLL sll;
     sll.push_front(2);
     sll.push_front(1);
@@ -21,8 +24,9 @@ int main() {
     cout << "SLL after pop_specific(3): ";
     sll.displayLL();
 
-
-    cout << "     Testing Doubly Linked List (DLL)   \n";
+    cout << "\n========================================\n";
+    cout << "   Testing Doubly Linked List (DLL)     \n";
+    cout << "========================================\n";
     DLL dll;
     dll.push_front(2);
     dll.push_front(1);
@@ -38,6 +42,25 @@ int main() {
     dll.pop_specific(3);
     cout << "DLL after pop_specific(3): ";
     dll.displayDLL();
+
+    cout << "\n========================================\n";
+    cout << "   Testing Circular Linked List (CLL)   \n";
+    cout << "========================================\n";
+    CLL cll;
+    cll.insertAtHead(2);
+    cll.insertAtHead(1);
+    cll.insertAtTail(3);
+    cll.insertAtTail(5);
+    cout << "CLL initial: ";
+    cll.displayCLL();
+
+    cll.insert(4, 4);
+    cout << "CLL after insert(4, 4): ";
+    cll.displayCLL();
+
+    cll.deleteSpecific(3);
+    cout << "CLL after deleteSpecific(3): ";
+    cll.displayCLL();
 
     return 0;
 }
